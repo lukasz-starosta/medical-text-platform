d4e29418faa0f12fc39e4c46dfc19a76cc3d3fe1
Michał Suliborski <msuliborski@gmail.com>
2021-06-08 22:32:29 +0200

    docker kinda work

--- backend/src/services/database_service.py
+++ backend/src/services/database_service.py
@@ -1,11 +1,11 @@
 import yaml
-from flask_mysqldb import MySQL
-
+from flask_mysql_connector import MySQL
+import os
 
 mysql = None
 
 def setup_db(app):
-    db = yaml.load(open('../db_config.yaml'))
+    db = yaml.load(open('src/services/db_config.yaml'))
     app.config['MYSQL_HOST'] = db['mysql_host']
     app.config['MYSQL_USER'] = db['mysql_user']
     app.config['MYSQL_PASSWORD'] = db['mysql_password']

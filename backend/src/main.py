9c438ce998656e907cfaeb4793a55fa7dae422cf
jedrek <jedrekszor@wp.pl>
2021-06-10 20:56:52 +0200

    fixed endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+from flask.json import jsonify
 from flask_cors import CORS
 import jwt
 import datetime
@@ -7,10 +8,12 @@ from controllers.entry_controller import create_entry
 from controllers.search_controller import search
 from controllers.login_controller import loginAndGenerateToken
 
-
 app = Flask(__name__)
 CORS(app)
 setup_db(app)
+
+message_ok = {'message': 'OK'}
+message_todo = {'message': 'TODO'}
 
 ############### AUTH ###############
 @app.route('/login', methods=['POST'])
@@ -19,35 +22,35 @@ def login():
 
 @app.route('/register', methods=['POST'])
 def register():
-    return "work in progress"
+    return jsonify(message_todo)
 
 @app.route('/check', methods=['POST'])
 def check():
-    return "work in progress"
+    return jsonify(message_todo)
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    return "work in progress"
+    return jsonify(message_todo)
 
 ############### BROWSER ###############
 @app.route('/search', methods=['GET'])
 def browse():
     content = request.args.get('query', type=str)
-    return search(content)
+    return jsonify(search(content))
 
 ############### ACCOUNT ###############
 @app.route('/user-info', methods=['GET'])
 def user_info():
     id = request.args.get('userId', type=int)
-    return "work in progress"
+    return jsonify(message_todo)
 
 @app.route('/change-password', methods=['POST'])
 def change_password():
-    return "work in progress"
+    return jsonify(message_todo)
 
 ############### ENTRY ###############
 @app.route('/create', methods=['POST'])
 def create():
     if create_entry(request.json):
-        return "OK"
+        return jsonify(message_ok)
     else: abort(406)

e20b14c2962b67574616b1dbbdeb2fa9aeae7c5e
jedrek <jedrekszor@wp.pl>
2021-06-10 21:15:20 +0200

    cleaning

--- shiit/app.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, request, abort
-
-app = Flask(__name__)
-# import declared routes
-import test_routes
--- shiit/test_routes.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-@app.route('/test', methods=['GET'])
-def test():
-    return 'it works!'

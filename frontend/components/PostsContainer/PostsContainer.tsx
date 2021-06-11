7ef4e707fa9c652a2c68e5e686e8d3fb68a2cb47
KrzysztofMoszczynski <luxtorpeda0104@gmail.com>
2021-06-11 14:59:53 +0200

    update

--- frontend/components/PostsContainer/PostsContainer.tsx
+++ frontend/components/PostsContainer/PostsContainer.tsx
@@ -2,16 +2,16 @@ import PostShort from '../PostShort/PostShort'
 import React, { FC } from 'react'
 import ShortPost from '../../shared/types/shortPost'
 
-/*interface Props {
-  postsList: Post[]
-}*/
+type PostList = {
+  postsList: ShortPost[]
+}
 
-const PostsContainer = (props: { postsList: Array<ShortPost> }) => {
-  console.log(typeof props.postsList + ' type of postList in ProfileBox')
+const PostsContainer = ({ postsList }: PostList) => {
+  console.log(typeof postsList + ' type of postList in ProfileBox')
   return (
     <div>
-      {props.postsList &&
-        props.postsList.map(post => {
+      {postsList &&
+        postsList.map(post => {
           ;<PostShort title={post.title} description={post.description} date={post.date} />
         })}
     </div>

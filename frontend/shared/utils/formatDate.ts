f1d16e4a317b2e1415446c8fefc40f60173f7d6b
lukasz-starosta <lstarosta98@gmail.com>
2021-06-13 21:02:26 +0200

    add unit tests

--- frontend/shared/utils/formatDate.ts
+++ frontend/shared/utils/formatDate.ts
@@ -3,5 +3,17 @@ export function formatDate(date: Date | string): string {
     date = new Date(date)
   }
 
-  return date.toLocaleDateString()
+  return `${getPrependedDay(date)}.${getPrependedMonth(date)}.${date.getFullYear()}`
+}
+
+export function getPrependedDay(date: Date): string {
+  return prependWithZero(date.getDate())
+}
+
+export function getPrependedMonth(date: Date): string {
+  return prependWithZero(date.getMonth() + 1)
+}
+
+export function prependWithZero(num: number): string {
+  return ('0' + num).slice(-2)
 }

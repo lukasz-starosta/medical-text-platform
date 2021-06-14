5cf74a6a10aec7262cdd53fbc17164ec051fb410
lukasz-starosta <lstarosta98@gmail.com>
2021-06-14 22:11:48 +0200

    integration tests

---
+++ frontend/components/LoginForm/index.spec.tsx
@@ -0,0 +1,28 @@
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import LoginForm from './LoginForm'
+
+describe('Tests of LoginForm component', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    })
+  })
+  it('renders errors when inputs are empty', async () => {
+    render(<LoginForm />)
+
+    fireEvent.click(screen.getByText('Zaloguj'))
+
+    expect(await screen.findByText('Wpisz nazwę użytkownika')).toBeInTheDocument()
+    expect(await screen.findByText('Wpisz hasło')).toBeInTheDocument()
+  })
+})

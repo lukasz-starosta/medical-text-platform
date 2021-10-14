db738d4b19935be6f0c19d02ed4c72e37fc7f0c7
lukasz-starosta <lstarosta98@gmail.com>
2021-10-14 10:17:41 +0200

    add cors

--- backend/src/main/java/com/chopaki/mtp/controller/EntryController.java
+++ backend/src/main/java/com/chopaki/mtp/controller/EntryController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost")
 @RequiredArgsConstructor
 public class EntryController {
     private final EntryService entryService;
--- backend/src/main/java/com/chopaki/mtp/controller/UserController.java
+++ backend/src/main/java/com/chopaki/mtp/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -16,6 +17,7 @@ import java.net.URI;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;

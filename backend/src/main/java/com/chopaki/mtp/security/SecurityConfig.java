ad9f09b38a24cf2ee987313d2308e940839cf8ef
jedrek <jedrekszor@wp.pl>
2021-10-15 11:07:23 +0200

    attempts to fix registration

--- backend/src/main/java/com/chopaki/mtp/security/SecurityConfig.java
+++ backend/src/main/java/com/chopaki/mtp/security/SecurityConfig.java
@@ -31,8 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/login").permitAll();
-        http.authorizeRequests().antMatchers("/register").permitAll();
+        http.authorizeRequests().antMatchers("/login*").permitAll();
+        http.authorizeRequests().antMatchers("/register*").permitAll();
         http.authorizeRequests().antMatchers("/actuator/prometheus").permitAll();
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));

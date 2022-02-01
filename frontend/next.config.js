cce748d8f31954b3badef7375cdfc1cc3c543f07
lukasz-starosta <lstarosta98@gmail.com>
2022-02-01 09:36:26 +0100

    Add pwa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,16 @@
-module.exports = {
-  webpackDevMiddleware: (config) => {
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+  webpackDevMiddleware: config => {
     config.watchOptions = {
       poll: 1000,
       aggregateTimeout: 300,
-    };
-    return config;
-  }
-}
+    }
+    return config
+  },
+})
